bffd0c9586c016b8801c51333291ca68a5efd8a4
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-24 21:27:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,11 @@ require 'pry'
 class String
 
   def sentence?
+<<<<<<< HEAD
     self.end_with?(".")
+=======
+    "Hi, my name is Sophie.".end_with?(".")
+>>>>>>> b6bd12944a0c7139b51d025e3ff3563af1a7466f
   end
 
   def question?
